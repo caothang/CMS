91d6fa074466493fccff9bf6ed9a9811a9cdd3a1
caothang <chuasonlam8484@gmail.com>
2012-06-13 23:05:55 +0700

    test git in new laptop

---
+++ cms/service/src/main/java/org/exoplatform/cms/service/CategoryProduct.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2012-2012 BigB.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see<http://www.gnu.org/licenses/>.
+ */
+package org.exoplatform.cms.service;
+
+/**
+ * Created by BigB 
+ * Author : Nguyen Van Thang
+ *          chuasonlam8484@gmail.com
+ * Jun 9, 2012  
+ */
+public class CategoryProduct {
+  String name;
+  String categoryId;
+//....
+}
